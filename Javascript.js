8c658dcde1dec1bfb80f6b59de9b2943e9769555
Tuul Chuluunsukh <tuulchuluunsukh@students.berkeley.net>
2016-06-03 10:25:19 -0700

    cdgyv

--- Javascript.js
+++ Javascript.js
@@ -6,6 +6,7 @@ var config = {
     storageBucket: "assassin-a488d.appspot.com",
 };
 firebase.initializeApp(config);
+var loggedInUser;
 
 function writeUserData(userId, name, email) {
     firebase.database().ref('users/' + userId).set({
@@ -19,7 +20,7 @@ function writeUserData(userId, name, email) {
     });
 }
 
-var loggedInUser;
+
 
 function createUser() {
     var userEmail = document.getElementById("userEmail").value;
@@ -45,52 +46,53 @@ function createUser() {
 //grabs all users from the database and returns a random one
 function chooseRandomUser() {
     var userArray = [];
-    firebase.database().ref('users/').on('value', function(snapshot) {
-        $.each(snapshot.val(), function(key, value) {
+    firebase.database().ref('users/').on('value', function (snapshot) {
+        $.each(snapshot.val(), function (key, value) {
             //do whatever
-            if(value.isAlive == true && value.isAssigned == false && value.username != loggedInUser.username) {
-                if(value.email != loggedInUser.email) {
+            if (value.isAlive == true && value.isAssigned == false && value.username != loggedInUser.username) {
+                if (value.email != loggedInUser.email) {
                     userArray.push(value);
                 }
-            };
-            var person = userArray[Math.floor(Math.random()*userArray.length)];
+            }
+            var person = userArray[Math.floor(Math.random() * userArray.length)];
             console.log(person.username);
             console.log(loggedInUser);
             document.getElementById("target").innerHTML = "Your target is " + person.username;
             document.getElementById("getTarget").disabled = true;
         });
 
-    })
+    });
+}
 
-    function logInUser() {
-        var userEmail = document.getElementById("userEmail").value;
-        var pin = document.getElementById("pin").value;
 
-        firebase.auth().signInWithEmailAndPassword(userEmail, pin);
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                // User is signed in.
-                loggedInUser = user;
-                firebase.database().ref('users/' + user.uid).on('value', function(snapshot) {
-                    console.log(snapshot.val().email);
-                    //doSomething(snapshot.val().email);
+function logInUser() {
+    var userEmail = document.getElementById("userEmail").value;
+    var pin = document.getElementById("pin").value;
 
-                });
-            }
-        });
-    }
+    firebase.auth().signInWithEmailAndPassword(userEmail, pin);
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            loggedInUser = user;
+            firebase.database().ref('users/' + user.uid).on('value', function(snapshot) {
+                console.log(snapshot.val().email);
+                //doSomething(snapshot.val().email);
 
-    function logOutUser() {
-        firebase.auth().signOut().then(function() {
-            // Sign-out successful.
-            // Sign-out successful.
-            console.log("Log Out: successful.");
-            alert("You are logged out now.");
-            return location.reload();
-        }, function(error) {
-            // An error happened.
-        });
-    }
+            });
+        }
+    });
+}
+
+function logOutUser() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        // Sign-out successful.
+        console.log("Log Out: successful.");
+        alert("You are logged out now.");
+        return location.reload();
+    }, function(error) {
+        // An error happened.
+    });
 }
 
 
